fb5a56cbbc1b0bc5cbed1a4d4ccadcc685864ab0
ShDw012 <hermawann012@gmail.com>
2026-06-26 13:38:12 +0700

    tambah kondisi

--- Bab 1 .py
+++ Bab 1 .py
@@ -1,5 +1,10 @@
-luas = 20
 
+print ("Hello Users")
+
+
+luas = 20
+count = 0
+target = 10000
 tinggi = 40
 
 satuan = "meter"
@@ -8,8 +13,9 @@ gg = luas ** tinggi
 
 hitung = hh / 2
 
-hasil1 = print("Luas: ", hh, satuan)
+hasil1 = print("Luas: \n", hh, satuan)
 
+# kondisi if else
 if (hh > 1000):
     print("Luas lebih dari 1000")
 elif (hh < 1000):
@@ -21,9 +27,21 @@ elif (gg > 1000):
 elif (gg < 1000):
     print("Pangkat luas kurang dari 1000")
 
+#kondisi dalam satu barus
 
+kondisi = "Besar" if hh >= 200 else "Kecil"
+print("Kondisi: ", kondisi, satuan)
+
+# math case
 match hitung:
     case 200 | 400 | 600:
         print ("saya makan")
     case 800:
         print("saya minum")
+
+# kondisi while
+
+while (tinggi < 100):
+    print("hitunggg : ", count)
+    count = count + 1
+print("Beres", target)
